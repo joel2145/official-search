0f3523152d65a12e3e39431106bf2416a5214598
KikuchiYuta <kikkun0513@gmail.com>
2022-02-23 10:54:52 +0900

    時刻のドロップダウンリストを作成

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,6 +5,7 @@ import {
   // FormErrorMessage,
   Input,
   Button,
+  Select
 } from '@chakra-ui/react'
 
 export const Form = () => {
@@ -13,6 +14,8 @@ export const Form = () => {
   const meetingIDChange = (e) => {
     setMeetingID(() => e.target.value)
   }
+
+
   return (
     <FormControl>
       <FormLabel htmlFor=''>ミーティングID</FormLabel>
@@ -20,7 +23,25 @@ export const Form = () => {
       <FormLabel htmlFor=''>パスコード</FormLabel>
       <Input id='passcode' type='text' placeholder='例 8a1SZu' size='md' />
       <FormLabel htmlFor=''>授業開始時刻</FormLabel>
-      <Input id='lessonTime' type='number' placeholder='12345678' size='md' />
+      <Select placeholder='年'>
+        <option value='option1'>2022</option>
+        <option value='option2'>2023</option>
+        <option value='option2'>2024</option>
+      </Select>
+      <Select placeholder='月'>
+        {[...Array(12)].map((_, i) => i + 1).map((value) => <option value='option1'>{value}</option>)}
+      </Select>
+      <Select placeholder='日付'>
+        {[...Array(31)].map((_, i) => i + 1).map((value) => <option value='option1'>{value}</option>)}
+      </Select>
+      <Select placeholder='時刻'>
+        <option value='option1'>2022</option>
+        <option value='option2'>2023</option>
+      </Select>
+      <Select placeholder='Select option'>
+        <option value='option1'>2022</option>
+        <option value='option2'>2023</option>
+      </Select>
       <Button onClick={() => alert("ボタンが押されました")} colorScheme='teal' size='md'>zoom授業を設定</Button>
     </FormControl>
   )
