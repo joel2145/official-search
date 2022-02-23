6d0c398a47db947ad540b8331757743efd03f282
KikuchiYuta <kikkun0513@gmail.com>
2022-02-23 09:54:00 +0900

    提出ボタンの見た目を作成

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   FormControl,
   FormLabel,
-  FormErrorMessage,
+  // FormErrorMessage,
   Input,
+  Button,
 } from '@chakra-ui/react'
 
 export const Form = () => {
+  const [meetingID, setMeetingID] = useState('')
+
+  const meetingIDChange = (e) => {
+    setMeetingID(() => e.target.value)
+  }
   return (
     <FormControl>
       <FormLabel htmlFor=''>ミーティングID</FormLabel>
-      <Input id='zoomID' type='number' placeholder='例 718 8545 7059' size='md' />
+      <Input id='zoomID' type='number' placeholder='例 718 8545 7059' size='md' value={meetingID} onChange={meetingIDChange} />
       <FormLabel htmlFor=''>パスコード</FormLabel>
-      <Input id='zoomID' type='number' placeholder='例 8a1SZu' size='md' />
+      <Input id='passcode' type='text' placeholder='例 8a1SZu' size='md' />
       <FormLabel htmlFor=''>授業開始時刻</FormLabel>
-      <Input id='zoomID' type='number' placeholder='12345678' size='md' />
+      <Input id='lessonTime' type='number' placeholder='12345678' size='md' />
+      <Button onClick={() => alert("ボタンが押されました")} colorScheme='teal' size='md'>zoom授業を設定</Button>
     </FormControl>
   )
 }
